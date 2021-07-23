7b363cac37031f785e3482fc08e469a60233b566
Osehi <osehiaseehilen@gmail.com>
2021-07-23 16:16:14 +0100

    'inital'

---
+++ app/src/main/java/com/polish/deploymotionlayout/CentralActivity.kt
@@ -0,0 +1,11 @@
+package com.polish.deploymotionlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CentralActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_central)
+    }
+}
